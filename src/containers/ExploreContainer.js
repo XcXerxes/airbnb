5d3d21dc9c79046f5466fe5ea21a818f21808637
XcXerxes <xcxerxes@aliyun.com>
2018-04-29 22:40:44 +0800

    add loggedIn

---
+++ src/containers/ExploreContainer.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export default class ExploreContainer extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'Explore',
+    tabBarIcon: ({tintColor}) => (
+      <Icon 
+        name="ios-search"
+        size={22}
+        color={tintColor}
+      />
+    )
+  }
+  render () {
+    return (
+      <View style={styles.wrapper}>
+        <Text>Explore Container</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    padding: 50
+  }
+})
--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -5,9 +5,11 @@ import { addNavigationHelpers, StackNavigator} from 'react-navigation'
 import LoggedOut from '../screens/LoggedOut'
 import LogIn from '../screens/LogIn'
 import ForgotPassword from '../screens/ForgotPassword'
+import LoggedIn from '../screens/LoggedIn'
 
 export const AppNavigator = StackNavigator({
   LoggedOut: {screen: LoggedOut},
+  LoggedIn: {screen: LoggedIn},
   LogIn: {screen: LogIn},
   ForgotPassword: {screen: ForgotPassword}
 })
