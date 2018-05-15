6ae8a65a4f782237484b97896cb20e3726b55b6a
XcXerxes <xia510377386@icloud.com>
2018-05-15 17:38:16 +0800

    'stars'

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -9,11 +9,39 @@ import {
 } from 'react-native'
 
 export default class Stars extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      stars: []
+    }
+  }
+  get stars () {
+    const {votes} = this.props
+    const starsNumner = parseInt(votes)
+    const starElements = []
+    for (let i=0; i < 5; i++) {
+      starElements.push(
+        <Icon 
+        name="star"
+        />
+      )
+    }
+  }
+
   render () {
     return (
       <View>
+        {this.stars}
         <Text>hello world!!!</Text>
       </View>
     )
   }
 }
+
+Stars.propTypes = {
+
+}
+
+const styles = StyleSheet.create({
+
+})
